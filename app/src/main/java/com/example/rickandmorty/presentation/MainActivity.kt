6447a8a858a0868be429e3ae6de7dac9bf15936b
19816728 <ASergeevnMartynova@sberbank.ru>
2022-07-05 14:13:11 +0300

    SplashScreen

--- app/src/main/java/com/example/rickandmorty/presentation/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/MainActivity.kt
@@ -16,20 +16,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_RICKAndMORTY)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        //
-//        supportActionBar?.hide()
-//
-//        Handler().postDelayed({
-//                val intent = Intent(SplashScreen(),
-//                    MainActivity::class.java)
-//            startActivity(intent)
-//            },3000)
-
-
-        //
 
         val fragmentCharacter = CharactersFragment()
         val fragmentLocations = LocationsFragment()
