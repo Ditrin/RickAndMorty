facc9b53bcc30e8cfba88ee8f5fbde166577e9b9
19816728 <ASergeevnMartynova@sberbank.ru>
2022-07-07 15:55:50 +0300

    grid

--- app/src/main/java/com/example/rickandmorty/presentation/fragments/DetailEpisodeFragment.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/fragments/DetailEpisodeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rickandmorty.R
 import com.example.rickandmorty.databinding.FragmentDetailEpisodesBinding
@@ -52,7 +53,7 @@ class DetailEpisodeFragment: Fragment() {
         viewModel.singleEpisode.observe(viewLifecycleOwner){
             binding.detailEpisodesRecyclerView.apply {
                 adapter = listAdapter
-                layoutManager = LinearLayoutManager(requireContext())
+                layoutManager = GridLayoutManager(activity, 2)
                 val dividerItemDecoration = DividerItemDecoration(
                     context,
                     (layoutManager as LinearLayoutManager).orientation
--- app/src/main/java/com/example/rickandmorty/presentation/fragments/DetailLocFragment.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/fragments/DetailLocFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rickandmorty.R
 import com.example.rickandmorty.databinding.FragmentDetailLocBinding
@@ -51,7 +52,7 @@ class DetailLocFragment : Fragment(R.layout.fragment_detail_loc) {
         viewModel.singleLocation.observe(viewLifecycleOwner){
             binding.detailLocationRecyclerView.apply {
                 adapter = listAdapter
-                layoutManager = LinearLayoutManager(requireContext())
+                layoutManager = GridLayoutManager(activity, 2)
                 val dividerItemDecoration = DividerItemDecoration(
                     context,
                     (layoutManager as LinearLayoutManager).orientation
